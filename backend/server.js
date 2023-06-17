0843a297b1803bbb66328b749eda67d066bd14ed
JayaKumar-2003 <jayakumar112234@gmail.com>
2023-06-17 21:37:43 +0530

    hosting

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import SeedRouter from "./Routes/SeedRouter.js";
 import cors from 'cors';
 import EmployeeRouter from './Routes/EmployeeRouter.js';
 import CustomerRouter from './Routes/CustomerRouter.js';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI).then(()=>{
@@ -25,6 +26,11 @@ app.use('/api/employee',EmployeeRouter);
 app.use('/api/customer',CustomerRouter);
 app.use((err,req,res,next)=> {
     res.status(500).send({ message:err.message});
+});
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
